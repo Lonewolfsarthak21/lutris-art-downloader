2ce34272c941af6b464cdc912253fa54bb1c481f
shresth sarthak awasthi <sarthak214003@gmail.com>
2025-07-21 15:33:21 +0530

    refactor: improve README and main.py structure, update requirements

--- README.md
+++ README.md
@@ -1,26 +1,28 @@
 # Lutris Cover Art Downloader
-
-This is an **EXTREMELY DIRTY** script to download cover art for Lutris games. It first started as a personal project, but I'm putting it here in case anyone else wants to use it.
-
-It comes from a long time bug in Lutris. The source used to get cover arts is... unreliable. It's not a big deal, but it's annoying. So I wrote this script to download the cover arts from SteamGridDB.
+Downloads Cover art,banners and icons for games in lutris
 
 ## Usage
 
 1. Clone the repository
+2. Create a virtual environment
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+```
 
-2. Install the dependencies
+3. Install the dependencies
 
 ```bash
 pip install -r requirements.txt
 ```
 
-3. Run the script
+4. Run the script
 
 ```bash
 python3 main.py
 ```
 
-> You need a SteamGridDB API key. You can get one [here](https://www.steamgriddb.com/profile/settings/api).
+> You need a SteamGridDB API key. You can get one [here](https://www.steamgriddb.com/profile/preferences/api).
 
 ## Screenshots
 
@@ -36,18 +38,7 @@ In a matter of seconds.
 
 ## How it works and warnings
 
-When I said the script dirty, it **IS** dirty. Obvisouly this won't get you any malware, but since I'm not that much into Python, I did what I could.
 What the script does is that it fetches the list of games from Lutris at `/home/$USER/.cache/lutris`, then it fetches the first cover art from SteamGridDB. It then saves the cover art in the Lutris cache folder.
-
-## Planned features
-
-- ❕ Argument support (--banner, --cover, --help)
-- ❕ Add a simple GUI with console output
-- ❕ Add a way to select the cover art you want via the GUI
-- Split in multiple files
-- Better code
-- Encode API Key in file
-- A better README?
 
 ## Credits
 
--- main.py
+++ main.py
@@ -4,27 +4,26 @@ import requests, sqlite3, os, inquirer, base64
 # Vars
 user = ''
 dbpath = ''
-dim = ''
+dims = []
 auth = ''
-covpath = ''
+covpaths = {}
+iconpath = ''
 
-# Main function
 def main():
-    global user, dbpath, dim, auth
-    print("Welcome " + user + " to Lutris Cover Art Downloader!\n")
+    global user, dbpath, dims, auth
+    print("Welcome to Lutris Cover Art & Icon Downloader!\n")
     user = GetUser()
-    dbpath = '/home/' + user + '/.local/share/lutris/pga.db'
-    dim = GetCoverType()
+    dbpath = f'/home/{user}/.local/share/lutris/pga.db'
+    dims = GetCoverType()
     auth = GetAPIKey()
     print("Getting API Key...\n")
     if auth == '':
         SetAPIKey()
+    PrepareDirectories()
     co = DBConnect()
     GetGamesList(co)
 
-
 ####### FUNCTIONS
-
 
 def GetUser():
     try:
@@ -34,22 +33,36 @@ def GetUser():
         exit(1)
 
 def GetCoverType():
-    global covpath
+    global covpaths, iconpath
     questions = [
-    inquirer.List('type',
-                    message="Would you like to download Steam banners or Steam vertical covers?",
-                    choices=['Banner (460x215)', 'Vertical (600x900)'],
-                ),
+        inquirer.List('type',
+            message="Which artwork would you like to download?",
+            choices=['Banner (920x430)', 'Vertical (600x900)', 'Both'],
+        ),
     ]
     ans = inquirer.prompt(questions)["type"]
     print('Cover type set to ' + ans + '\n')
-    if ans == 'Banner (460x215)':
-        covpath = '/home/' + user + '/.cache/lutris/banners/'
-        dim = '460x215'
+    userhome = f'/home/{user}'
+    covpaths = {
+        'banner': f'{userhome}/.cache/lutris/banners/',
+        'vertical': f'{userhome}/.cache/lutris/coverart/'
+    }
+    iconpath = f'{userhome}/.cache/lutris/icons/'
+
+    if ans == 'Banner (920x430)':
+        return [('banner', '920x430')]
+    elif ans == 'Vertical (600x900)':
+        return [('vertical', '600x900')]
     else:
-        covpath = '/home/' + user + '/.cache/lutris/coverart/'
-        dim = '600x900'
-    return dim
+        return [
+            ('banner', '920x430'),
+            ('vertical', '600x900')
+        ]
+
+def PrepareDirectories():
+    for p in covpaths.values():
+        os.makedirs(p, exist_ok=True)
+    os.makedirs(iconpath, exist_ok=True)
 
 def SaveAPIKey(key):
     with open('./apikey.txt', 'w') as f:
@@ -58,25 +71,25 @@ def SaveAPIKey(key):
 def GetAPIKey():
     if os.path.isfile('./apikey.txt'):
         with open('./apikey.txt', 'r') as f:
-            key = f.read()
-            auth = {'Authorization': 'Bearer ' + key}
-            return auth
+            key = f.read().strip()
+            return {'Authorization': 'Bearer ' + key}
     else:
         return ''
 
 def SetAPIKey():
+    global auth
     print("Could not find API key")
-    print('You need a SteamGriDB API key to use this script.')
+    print('You need a SteamGridDB API key to use this script.')
     print('You can get one by using your Steam account and heading here: https://www.steamgriddb.com/profile/preferences/api\n')
     api = input("Enter your SteamGridDB API key: ")
-    auth = {'Authorization': 'Bearer ' + api}
+    auth = {'Authorization': 'Bearer ' + api.strip()}
     TestAPI(auth, api)
 
 def TestAPI(key, api):
     r = requests.get('https://www.steamgriddb.com/api/v2/grids/game/1?dimensions=600x900', headers=key)
     if r.status_code == 200:
-        print("API key is valid, saving...")
-        SaveAPIKey(api)
+        print("API key is valid, saving...\n")
+        SaveAPIKey(api.strip())
     else:
         print("API key is invalid")
         exit(1)
@@ -89,42 +102,76 @@ def DBConnect():
         exit(1)
     return conn
 
-# Search for a game by name via Lutris database, then get the grid data
 def SearchGame(game):
     res = requests.get('https://www.steamgriddb.com/api/v2/search/autocomplete/' + game, headers=auth).json()
     if len(res["data"]) == 0:
-        print("Could not find a cover for game " + game)
+        print(f"[!] Could not find SGDB entry for '{game}'")
+        return None
     else:
-        print("Found game " + game.replace('-', ' ').title())
-        id = res["data"][0]["id"]
-        return id
+        print(f"[+] Found SGDB entry for: {game.replace('-', ' ').title()}")
+        return res["data"][0]["id"]
 
-# Download cover by searching for the game via its name, then via its SteamGriDB's ID
-def DownloadCover(name):
+def DownloadCover(name, dimension, subpath):
     gameid = SearchGame(name)
-    print("Downloading cover for " + name.replace('-', ' ').title())
-    grids = requests.get('https://www.steamgriddb.com/api/v2/grids/game/' + str(gameid) + '?dimensions=' + dim, headers=auth).json()
+    if gameid is None:
+        return
+    print(f"[*] Downloading {dimension} cover for {name.replace('-', ' ').title()}")
+    grids = requests.get(
+        f'https://www.steamgriddb.com/api/v2/grids/game/{gameid}?dimensions={dimension}', headers=auth).json()
+    url = None
     try:
         url = grids["data"][0]["url"]
-    except:
-        print("Could not find a cover for game " + name)
-        return
+    except (KeyError, IndexError):
+        if dimension == '920x430':
+            grids = requests.get(
+                f'https://www.steamgriddb.com/api/v2/grids/game/{gameid}?dimensions=460x215', headers=auth).json()
+            try:
+                url = grids["data"][0]["url"]
+            except (KeyError, IndexError):
+                print(f"[!] No cover found for {name} at 920x430 or 460x215")
+                return
+        else:
+            print(f"[!] No cover found for {name}")
+            return
+
+    filename = os.path.join(covpaths[subpath], name + '.jpg')
     r = requests.get(url)
-    with open(covpath + name + '.jpg', 'wb') as f:
+    with open(filename, 'wb') as f:
         f.write(r.content)
 
-# Get all games and for each game, check if it already has a cover
+def DownloadIcon(name):
+    gameid = SearchGame(name)
+    if gameid is None:
+        return
+    print(f"[*] Downloading icon for {name.replace('-', ' ').title()}")
+    icons = requests.get(f'https://www.steamgriddb.com/api/v2/icons/game/{gameid}', headers=auth).json()
+    try:
+        url = icons["data"][0]["url"]
+    except (KeyError, IndexError):
+        print(f"[!] No icon found for {name}")
+        return
+    r = requests.get(url)
+    with open(os.path.join(iconpath, name + '.png'), 'wb') as f:
+        f.write(r.content)
+
 def GetGamesList(co):
     c = co.execute('SELECT slug FROM games')
     games = c.fetchall()
     for entry in games:
         title = entry[0]
-        if not os.path.isfile(covpath + title + '.jpg'):
-            # If not, download it
-            DownloadCover(title)
+        for subpath, dim in dims:
+            cover_file = os.path.join(covpaths[subpath], title + '.jpg')
+            if not os.path.isfile(cover_file):
+                DownloadCover(title, dim, subpath)
+            else:
+                print(f"[✓] {dim} Cover for {title} already exists")
+        icon_file = os.path.join(iconpath, title + '.png')
+        if not os.path.isfile(icon_file):
+            DownloadIcon(title)
         else:
-            print("Cover for " + title.replace('-', ' ').title() + " already exists")
-    print('All done ! Restart Lutris for the changes to take effect')
+            print(f"[✓] Icon for {title} already exists")
+
+    print('\nAll done! Restart Lutris for the changes to take effect.')
 
 if __name__ == '__main__':
     main()
